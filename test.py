4a2548d80538cf06431721dbec3ed9de266d0b15
ssmsaito <saito@iflab.co.jp>
2021-03-26 13:44:26 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import asyncio
+from ducts import Duct
+
+class MyPlayground:
+    def __init__(self):
+        self.duct = Duct()
+
+    async def catchall_event_handler(self, rid, eid, data):
+        print(eid, data) 
+
+    async def on_open(self, event):
+        await self.duct.send(self.duct.next_rid(), self.duct.EVENT["LIST_PROJECTS"], None)
+
+    async def main(self):
+        self.duct.connection_listener.onopen = self.on_open
+        self.duct.catchall_event_handler = self.catchall_event_handler
+        await self.duct.open("http://localhost/ducts/wsd")
+
+if __name__=="__main__":
+    pg = MyPlayground()
+    asyncio.run(pg.main())
